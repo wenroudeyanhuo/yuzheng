6407da48c4c6e5e941ba64770979a190e79e9450
monan <1657422582@qq.com>
2022-11-01 18:56:36 +0800

    更新

---
+++ src/views/test2/components/show.vue
@@ -0,0 +1,206 @@
+<template>
+
+  <div  style="background:transparent;position: relative;top:-50px;">
+    <button class="first" style="position: relative;top:1px;right: -40px" @click="isActive = 0">事情详情</button>
+    <button class="second" style="position: relative;top:1px;right: -180px" @click="isActive = 1">实时监控</button>
+
+    <div class="first1" v-if="isActive == 0">
+      <span style="position:relative;left:44px;top:-23px;color: white">事情详情</span>
+
+
+<!--      下面图片列表，横向展示-->
+      <template>
+      <el-scrollbar>
+        <div class="scrollbar-flex-content">
+          <p v-for="item in 50" :key="item" class="scrollbar-demo-item">
+            {{ item }}
+          </p>
+        </div>
+      </el-scrollbar>
+      </template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div style="position: relative;right: 220px;top:-12px">
+
+      <div class="aside" style="display:inline;float: right;">
+        <div style="margin-left: 20px;margin-top: 20px;">
+          <el-image style="vertical-align:middle;" :src="require('@/assets/icon_jiankong.png')"></el-image>
+          <span class="span6">nb市</span>
+        </div>
+
+
+        <div :style="background2" style="height:40px;width: 450px;margin-left: 20px">
+          <!--         vertical-align:middle;图片和文字居中 -->
+          <el-image style="width: 30px; height: 27px;margin-left:8px;vertical-align:middle;" :src="require('@/assets/6.png')" fit="fill" ></el-image>
+          <span class="span7">处理详情</span>
+        </div>
+        <!-- 描述列表       -->
+        <!-- 这里的所有数据，后台交互改变数据       -->
+        <!--      direction="vertical" 是指文字的排列方式  -->
+        <el-descriptions  :column="1" :span="2">
+          <el-descriptions-item label="预警类型"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >疑似船只</el-descriptions-item>
+          <el-descriptions-item label="预警方位"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          > A5</el-descriptions-item>
+          <el-descriptions-item label="预警时间"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >2022-08-06 13:40:02</el-descriptions-item>
+          <el-descriptions-item label="预警状态"
+                                label-class-name="my-label1"
+          ><el-tag size="small">待处理</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="推送时间"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >2022-08-06 13:40:02</el-descriptions-item>
+          <el-descriptions-item label="负责人"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >田七</el-descriptions-item>
+          <el-descriptions-item label="联系方式"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >12345678909</el-descriptions-item>
+          <el-descriptions-item label="推送内容"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >有疑似报警</el-descriptions-item>
+          <el-descriptions-item label="反馈信息"
+                                label-class-name="my-label"
+                                content-class-name="my-content"
+          >-</el-descriptions-item>
+        </el-descriptions>
+        </div>
+      </div>
+
+    </div>
+    <div class="second2" v-else-if="isActive == 1">
+
+
+      <span style="position:relative;left:44px;top:-23px;color: white">实时监控</span>
+    </div>
+
+
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Show',
+  data()
+  {
+    return{
+      background2: {
+        // 背景图片地址
+        backgroundImage: 'url(' + require('@/assets/bg_title@2x.png') + ')',
+        // 背景图片是否重复
+        backgroundRepeat:'no-repeat',
+        // 背景图片大小
+        backgroundSize: '60%',
+        // 背景颜色
+        // 背景图片位置
+        // backgroundPosition: 'center top'
+      },
+      isActive: -1,
+    }
+  }
+}
+
+
+</script>
+
+<style  lang="scss" scoped>
+.scrollbar-flex-content {
+  display: flex;
+}
+.scrollbar-demo-item {
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-danger-light-9);
+  color: var(--el-color-danger);
+}
+::v-deep .el-descriptions__body {
+  background: transparent;
+  margin-left: 60px;
+  margin-top: 30px;
+  /*  color:#dce7e7;*/
+}
+::v-deep .my-label{
+  margin-bottom: 5px;
+  color: #3fd9d5;
+
+}
+::v-deep .my-label1{
+  margin-bottom: 30px;
+  color: #3fd9d5;
+}
+::v-deep .my-content{
+  margin-bottom: 5px;
+  color:#dce7e7;
+}
+.span6{
+  color:#dce7e7;
+  /*解决父子元素margin叠加问题*/
+  display:inline-block;
+  margin: 30px 0px 30px 10px;
+}
+.span7{
+  /*  !*解决父子元素margin叠加问题*!
+    display:inline-block;*/
+  margin-left:40px;
+  /*  margin-bottom: 30px;*/
+  line-height: 40px;
+  color:#dce7e7;
+  font-size: 18px;
+}
+.aside{
+  height: 800px;
+  width: 400px;
+  background-color: rgb(12, 37, 67,0.7);
+}
+.first{
+  position: relative;
+  color:#d9effe;
+  background: transparent;
+  border: none;
+  margin-left: 800px;
+  top:-10px;
+
+}
+.second{
+  top:-10px;
+  position: relative;
+  color:#d9effe;
+  background: transparent;
+  border: none;
+}
+
+
+
+
+</style>
---
+++ src/views/test2/index.vue
@@ -0,0 +1,257 @@
+<template>
+  <el-row>
+    <el-col :span="5">
+      <el-card class="box-card">
+        <div  style="margin:20px;width: 300px;height: 40px;position:relative;top:-10px;left: -20px" class="clearfix" :style="background">
+          <!--          style="border: none" align="center"-->
+          <el-image style="width: 30px;height: 30px ;position: relative;left: 10px;top:3px" :src="require('@/assets/6.png')" fit="fill"></el-image>
+          <span style="position:absolute;color:white;left:120px;top:10px">预警事件</span>
+          <el-select   v-model="ruleForm.cities" :popper-append-to-body="false"  clearable prop-class="select-down" placeholder="类型"  class="select_shijian" >
+            <el-option
+              v-for="item in cities"
+              :key="item.value"
+              :label="item.label"
+              :value="item"
+            >
+              <span style="float: left">{{ item.label }}</span>
+              <span style="float: right;font-size: 13px;">{{ item.value }}</span>
+            </el-option>
+          </el-select>
+          <el-select v-model="ruleForm.shebei"  :popper-append-to-body="false"  clearable prop-class="select-down" placeholder="设备"  class="select_shebei">
+            <el-option
+              v-for="item in shebei"
+              :key="item.value"
+              :label="item.label"
+              :value="item">
+              <span style="float: left">{{ item.label }}</span>
+              <span style="float: right;font-size: 13px">{{ item.value }}</span>
+            </el-option>
+          </el-select>
+
+
+<!--          列表-->
+          <el-card class="box-card3">
+            <div>
+              <ul class="ul1">
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                         文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                         文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                           文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                              文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                              文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <span class="span">文字                                             文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                           文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                           文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+                <li class="li1">
+                  <div>
+                    <span class="span">文字                                         文字1</span>
+                    <el-image style="width:260px; height: 235px;" :src="require('@/assets/yujing.jpg')" fit="fill" ></el-image>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </el-card>
+
+
+
+
+
+
+        </div>
+      </el-card>
+    </el-col>
+    <el-col :span="19">
+      <el-card class="box-card1">
+        <!--        头-->
+        <div style="background-color:#003377;top:10px;width: 1200px;height: 40px;margin-left: -10px">
+          <el-image style="width: 60px;height: 60px ;position: relative;top:-5px;left: -18px" :src="require('@/assets/icon_title_dianwei@2x.png')" fit="fill"></el-image>
+          <div style=" height: 800px;width: 1400px;">
+            <!-- 背景色有问题 -->
+                <Show msg=""/>
+              </div>
+              <el-image  style="position:relative;top:10px;right:-10px;width: 30px;height: 30px" id="Closehide" @click="Closehide_onclick" :src="require('@/assets/chahao.png')" fit="fill"></el-image>
+            </div>
+
+
+
+
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+<style  lang="scss"  scoped>
+//识别空格
+.span{
+  color:#FFFFFF;
+  white-space: pre;
+  font-weight:bold;
+}
+.ul1 {
+  position: relative;
+  width: 400px;
+  height: 500px;
+  margin-left: 0px;
+  overflow: scroll;
+}
+.li1 {
+  color: #FFFFFF;
+
+  width:230px;
+  //line-height: 50px;
+}
+::v-deep .box-card3{
+  z-index: 100;
+  position: relative;
+  top: 50px;
+  left: -40px;
+
+  height:900px;
+  width: 600px;
+
+  border: 3px;
+  border-color: #FFFFFF;
+  background-color:#000088 ;
+  padding: 1px 1px;
+
+}
+.box-card3 ::v-deep .el-card__body{
+  padding: 0px 0px;
+
+}
+::v-deep
+//下拉框面板
+.el-select-dropdown{
+  font-size: 30px;
+  color: black;
+//background-color:transparent;
+}
+::v-deep .el-popper{
+  color: black;
+}
+::v-deep .el-select-dropdown__item{
+  color: black;
+}
+//选中时的样式
+  ::v-deep .el-select-dropdown__item.hover{
+    background: transparent;
+  }
+::v-deep .el-scrollbar{
+  color: black;
+}
+.select_shijian{
+//color: #FFFFFF;
+
+  background: transparent;
+  width: 90px;
+  position: relative;
+  right: -150px;
+  top:50px;
+//right: 0px;
+
+}
+
+.select_shebei{
+  background: transparent;
+  width: 90px;
+  position: relative;
+  top:50px;
+  right: 50px;
+
+
+}
+.box-card {
+
+//margin-bottom: 20px;
+  background-color:#000088 ;
+  opacity:0.7;
+  height:700px;
+  width: 300px;
+
+  padding: 0px 0px;
+  border: none;
+  z-index: 100;
+}
+.box-card1 {
+  z-index: 100;
+
+  margin-bottom: 20px;
+  background-color:#000088 ;
+  opacity:0.7;
+  height:700px;
+  width: 1170px;
+
+  padding: 0px 0px;
+  border: none;
+}
+</style>
+
+
+<script>
+import Show from "@/views/test2/components/show";
+export default {
+  name: "test2",
+  components:{
+  Show,
+  },
+  data() {
+    return {
+      cities:[{'value':'疑似船只','label':'事件类型'},{'value':'测试船只','label':'事件类型'},{'value':'确认船只','label':'事件类型'}],
+      shebei:[{'value':'设备1','label':'1'},{'value':'设备2','label':'2'},{'value':'设备3','label':'3'}],
+      ruleForm:{
+        shebei:{},
+        cities:{},
+      },
+      background: {
+        // 背景图片地址
+        backgroundImage: 'url(' + require('../../../public/bg_title@2x.png') + ')',
+        // 背景图片是否重复
+        // 背景图片大小
+        backgroundSize: 'cover',
+        // 背景颜色
+        // 背景图片位置
+        backgroundPosition: 'center top'
+      },
+    }
+  }
+}
+</script>
